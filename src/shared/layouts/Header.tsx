f04d3a9b9b2d7f3aa24c72b3b01221e16d42016e
Vitor Gessner <gessnervgg@gmail.com>
2026-06-02 00:59:48 -0300

    Making user stats component and including it on a tab in profile

--- src/shared/layouts/Header.tsx
+++ src/shared/layouts/Header.tsx
@@ -72,7 +72,7 @@ const Header = () => {
                 </div>
                 <div></div>
                 {!isPending && (
-                    <div className="absolute right-5 flex items-center justify-right gap-5">
+                    <div className="absolute right-5 flex items-center justify-right gap-5 text-navy tracking-tight font-heading font-semibold">
                         {isAuthenticated && <Link to={'/guess'}>Guess</Link>}
                         {isAuthenticated && (
                             <button className="cursor-pointer">
