58eb16bdc9b991c9e8c96bcce544777f925b1897
Vitor Gessner <gessnervgg@gmail.com>
2026-06-11 02:25:55 -0300

    Made sidebar more visible and added version and note on website

--- src/features/game/config/components/Config.tsx
+++ src/features/game/config/components/Config.tsx
@@ -2,12 +2,21 @@ import { useState } from 'react';
 import { Switch } from '@/components/ui/switch';
 import Form from '@/features/auth/components/form/Form';
 import useGuessStore from '../../guess/stores/useGuessStore';
-import { ChevronsRight } from 'lucide-react';
+import { ChevronsLeft, ChevronsRight } from 'lucide-react';
 
 const ConfigComponent = ({ inSheet }: { inSheet?: boolean }) => {
-    const [isExpanded, setIsExpanded] = useState<boolean>(false);
-    const { config, setConfig } = useGuessStore();
     const { isGuessed } = useGuessStore();
+    const { config, setConfig } = useGuessStore();
+    const [isPinned, setIsPinned] = useState<boolean>(false);
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+    const [prevIsGuessed, setPrevIsGuessed] = useState<boolean>(isGuessed);
+    const isExpanded = isPinned || isHovered;
+
+    if (prevIsGuessed !== isGuessed) {
+        setPrevIsGuessed(isGuessed);
+        if (isGuessed) setIsPinned(true);
+        if (!isGuessed) setIsPinned(false);
+    }
 
     if (inSheet) {
         return (
@@ -53,7 +62,9 @@ const ConfigComponent = ({ inSheet }: { inSheet?: boolean }) => {
                             id="tracklist"
                             disabled={!isGuessed}
                             defaultChecked={config.tracklist}
-                            onCheckedChange={() => setConfig({ ...config, tracklist: !config.tracklist })}
+                            onCheckedChange={() =>
+                                setConfig({ ...config, tracklist: !config.tracklist })
+                            }
                         />
                     </div>
                 </Form.Label>
@@ -67,13 +78,13 @@ const ConfigComponent = ({ inSheet }: { inSheet?: boolean }) => {
     return (
         <div
             className="fixed lg:absolute left-0 lg:top-12 bottom-0 z-9 min-w-dvw w-full lg:min-w-fit lg:w-fit lg:pr-60 text-(--text) bg-(--card-light) lg:bg-transparent"
-            onMouseEnter={() => setIsExpanded(true)}
-            onMouseLeave={() => setIsExpanded(false)}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
         >
             <aside
-                className={`flex flex-col left-0 ${!isGuessed ? '-mb-70' : '-mb-58'} pb-10 h-full min-w-dvw w-full lg:w-fit lg:min-w-fit lg:-ml-60 transition-all max-w-66 bg-(--card-light) border-t-2 border-border lg:bg-(--card-light) duration-400 ease-in-out ${isExpanded && (`lg:translate-x-60 lg:translate-y-0 ${!isGuessed ? '-translate-y-70' : '-translate-y-50'}`)}`}
+                className={`flex flex-col left-0 ${!isGuessed ? '-mb-70' : '-mb-58'} pb-10 h-full min-w-dvw w-full lg:w-fit lg:min-w-fit lg:-ml-60 transition-all max-w-66 bg-(--card-light) border-t-2 border-border lg:bg-(--card-light) duration-400 ease-in-out translate-x-8 ${isExpanded && `lg:translate-x-60 lg:translate-y-0 ${!isGuessed ? '-translate-y-70 lg:-translate-y-60' : '-translate-y-50 lg:-translate-y-70'}`}`}
             >
-                <div className="flex flex-col mx-auto pt-3 px-10 gap-3 text-lg bg-(--card-light) lg:bg-(--card-light) border-border">
+                <div className="flex flex-col mx-auto pt-3 px-5 gap-3 text-lg bg-(--card-light) lg:bg-(--card-light) border-border">
                     <h2 className="text-2xl">Guessing options</h2>
                     <Form.Label className="mt-4 w-full">
                         <div className="flex flex-row items-center justify-between w-full">
@@ -82,7 +93,9 @@ const ConfigComponent = ({ inSheet }: { inSheet?: boolean }) => {
                                 id="artist"
                                 disabled={!isGuessed}
                                 defaultChecked={config.artist}
-                                onCheckedChange={() => setConfig({ ...config, artist: !config.artist })}
+                                onCheckedChange={() =>
+                                    setConfig({ ...config, artist: !config.artist })
+                                }
                             />
                         </div>
                     </Form.Label>
@@ -93,7 +106,9 @@ const ConfigComponent = ({ inSheet }: { inSheet?: boolean }) => {
                                 id="genre"
                                 disabled={!isGuessed}
                                 defaultChecked={config.genre}
-                                onCheckedChange={() => setConfig({ ...config, genre: !config.genre })}
+                                onCheckedChange={() =>
+                                    setConfig({ ...config, genre: !config.genre })
+                                }
                             />
                         </div>
                     </Form.Label>
@@ -115,7 +130,9 @@ const ConfigComponent = ({ inSheet }: { inSheet?: boolean }) => {
                                 id="tracklist"
                                 disabled={!isGuessed}
                                 defaultChecked={config.tracklist}
-                                onCheckedChange={() => setConfig({ ...config, tracklist: !config.tracklist })}
+                                onCheckedChange={() =>
+                                    setConfig({ ...config, tracklist: !config.tracklist })
+                                }
                             />
                         </div>
                     </Form.Label>
@@ -123,11 +140,22 @@ const ConfigComponent = ({ inSheet }: { inSheet?: boolean }) => {
                         {!isGuessed && 'You need to answer first before changing config'}
                     </span>
                 </div>
-                <div className="flex h-full relative bg-(--card-light)">
-                    <ChevronsRight
-                        size={30}
-                        className="lg:absolute right-0 rounded-xs bg-(--card-light) hidden lg:block"
-                    />
+                <div className="flex h-full relative bg-(--card-light) rounded-md">
+                    {!isExpanded ? (
+                        <ChevronsRight
+                            size={30}
+                            className="lg:absolute h-20 w-8 -right-7 rounded-xs bg-(--card-light) hidden lg:block cursor-pointer"
+                        />
+                    ) : (
+                        <ChevronsLeft
+                            size={30}
+                            className="lg:absolute h-20 w-8 -right-7 rounded-xs bg-(--card-light) hidden lg:block cursor-pointer"
+                            onClick={() => {
+                                setIsPinned(false);
+                                setIsHovered(false);
+                            }}
+                        />
+                    )}
                 </div>
             </aside>
         </div>
